ec4907c04b3fede7e1b8b648419019677bc8f3a7
user <aroosa.ansar@9dtechnologies.com>
2022-08-01 05:51:59 -0700

    first commit from development branch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,5 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate: change one")
+
+        Log.d(TAG, "onCreate: change two")
     }
 }
